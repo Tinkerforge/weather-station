4ca016c632c9afec172d09a8a2e5e517c90bd07c
Matthias Bolte <matthias@tinkerforge.com>
2013-05-16 16:16:09 +0200

    demo: Update _build_dmg.sh script

--- demo/src/_build_dmg.sh
+++ demo/src/_build_dmg.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-dot_version=`grep BRICKV_VERSION config_common.py | sed -e 's/BRICKV_VERSION = "\(.*\)"/\1/'`
+dot_version=`grep DEMO_VERSION config.py | sed -e 's/DEMO_VERSION = "\(.*\)"/\1/'`
 underscore_version=`printf %s $dot_version | sed -e 's/\./_/g'`
-dmg=brickv_macos_$underscore_version.dmg
+dmg=starter_kit_weather_station_demo_macos_$underscore_version.dmg
 rm $dmg
-hdiutil create -fs HFS+ -volname "Brickv-$dot_version" -srcfolder dist $dmg
+hdiutil create -fs HFS+ -volname "Starter-Kit-Weather-Station-Demo-$dot_version" -srcfolder dist $dmg
